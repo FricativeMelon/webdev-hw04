21c9e92c8f3ea22d011f263e18bf7ff27dd16a2b
Memory Server App <memoryserver@vultr.guest>
2018-10-04 22:49:22 +0000

    Updated for deploy, not working yet

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Memory.MixProject do
   def project do
     [
       app: :memory,
-      version: "0.1.0",
+      version: "0.1.5",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
